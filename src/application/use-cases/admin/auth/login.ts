1ad835fab7de72380c4d92b37590072e57ba1ac4
Abbas firdous <abbasfathima40@gmail.com>
2025-09-08 13:05:12 +0530

    chore(admin): admin login use-case code format

--- src/application/use-cases/admin/auth/login.ts
+++ src/application/use-cases/admin/auth/login.ts
@@ -1,15 +1,14 @@
-import { env } from "../../../../infrastructure/config/env.js"
+import { env } from "../../../../infrastructure/config/env.js";
 
-export class AdminLoginUC{
-    constructor(){}
+export class AdminLoginUC {
+  constructor() {}
 
-    execute = (email: string, password: string) => {
-
-        if(email === env.admin_email && password === env.admin_password){
-            // generate token
-            return {success: true, message: 'login successful'}
-        }else{
-            throw new Error('Invalid Admin Credentials. Please try again!');
-        }
+  execute = (email: string, password: string) => {
+    if (email === env.admin_email && password === env.admin_password) {
+      // generate token
+      return { success: true, message: "login successful" };
+    } else {
+      throw new Error("Invalid Admin Credentials. Please try again!");
     }
+  };
 }
